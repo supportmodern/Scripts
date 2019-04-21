6267c6ceeb2336aba9576e3231c23d8633df939e
Nathan Bridges <48260902+supportmodern@users.noreply.github.com>
2019-04-21 16:17:01 -0500

    Add files via upload
    Modified PowerShell script from here (lines 21,22) to ensure full command line would run as the default is truncated. https://blogs.technet.microsoft.com/askpfeplat/2018/12/18/rsat-on-windows-10-1809-in-disconnected-environments/
    
    This script allows the Windows 10 1809+ (1903 in the script) RSAT installation using Feature On Demand and Offline (no internet needed).

---
+++ W10_RSAT_FOD_Offline/W10_FOD_RSAT_Offline_CopySource.ps1
@@ -0,0 +1,32 @@
+#Specify ISO Source location
+
+$FoD_Source = "$env:USERPROFILE\Downloads\W10RSAT_FOD\1903\1903_FoD_Disk1.iso"
+
+#Mount ISO
+
+Mount-DiskImage -ImagePath "$FoD_Source"
+
+$path = (Get-DiskImage "$FoD_Source" | Get-Volume).DriveLetter
+
+#Language desired
+
+$lang = "en-US"
+
+#RSAT folder 
+
+$dest = New-Item -ItemType Directory -Path "$env:SystemDrive\temp\RSAT_1903_$lang" -force
+
+#get RSAT files 
+
+Get-ChildItem ($path+":\") -name -recurse -include *~amd64~~.cab,*~wow64~~.cab,*~amd64~$lang~.cab,*~wow64~$lang~.cab -exclude *languagefeatures*,*Holographic*,*NetFx3*,*OpenSSH*,*Msix* |
+ForEach-Object {copy-item -Path ($path+“:\”+$_) -Destination $dest.FullName -Force -Container}
+
+#get metadata
+
+copy-item ($path+":\metadata") -Destination $dest.FullName -Recurse
+
+copy-item ($path +“:\"+“FoDMetadata_Client.cab”) -Destination $dest.FullName -Force -Container
+
+#Dismount ISO
+
+Dismount-DiskImage -ImagePath "$FOD_Source"
